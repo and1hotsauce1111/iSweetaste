bb01fc13ba7053534a52251696832ae83215d7f2
and1hotsauce1111 <and1hotsauce1111@gmail.com>
2020-06-01 16:36:33 +0800

    test window resize2

--- components/chat/index.vue
+++ components/chat/index.vue
@@ -79,8 +79,10 @@ export default {
   mounted() {
     const resizeHandler = () => {
       if (window.innerHeight < 568) {
-        this.$refs.chatMessage.style.height = '50%'
+        this.$refs.chatMessage.style.height = window.innerHeight / 2 + 'px'
+        return false
       }
+      this.$refs.chatMessage.style.height = '80%'
     }
     window.addEventListener('resize', resizeHandler)
   },
