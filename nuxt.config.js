294f3f6d29d301acd8210cd3ea435c488538aacd
and1hotsauce1111 <and1hotsauce1111@gmail.com>
2020-07-02 22:12:27 +0800

    Fixed prevent user scale2

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0'
+        content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
       },
       {
         hid: 'description',
